4118b3548e2959c2ab084c2ecd1a0ccb961108fe
paulchino <paulchino@gmail.com>
2015-06-08 23:03:33 -0700

    first commit

---
+++ sorting.js
@@ -0,0 +1,84 @@
+//************ SORING ALGORITHMS
+
+//------------ test cases
+var arr1 = [0];
+var arr2 = [0,1,2];
+var arr3 = [-3,2,122,100];
+var arr4 = [3,0,-3.2,-100];
+
+
+//------------ BUBBLE SORT 
+
+// worst case O(n^2)
+// best case O(n)
+function bubble(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		var flag = true;
+		for (var j = 0; j < arr.length - i - 1; j++ ) {
+			if (arr[j] > arr[j+1]) {
+				// swap
+				var temp = arr[j+1];
+				arr[j+1] = arr[j];
+				arr[j] = temp;
+				flag = false;
+			}
+		}
+		if (flag) {
+			return arr
+		}
+	}
+	return arr
+}
+
+
+//----------- SELECTION SORT
+//advantages 
+	//in-place sort (requires no additional storage space)
+
+//worst case O(n^2)
+//best case O(n)
+
+function selection(arr) {
+	var min, temp;
+
+	for (var i = 0; i < arr.length-1; i++) {
+		min = i;
+		for(var j = i; j < arr.length; j++) {
+			//find the index of the min value
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		//swap
+		temp = arr[i];
+		arr[i] = arr[min];
+		arr[min] = temp;
+	}
+	return arr
+}
+
+
+//----------- INSERTION SORT
+//generally more efficent than selection and bubble
+function insertion(arr) {
+	var j, v;
+	for (var i = 1; i < arr.length; i++) {
+		v = arr[i];
+		j = i;
+		while(arr[j-1] > v && j>0) {
+			arr[j] = arr[j-1];
+			j--;
+		}
+		arr[j] = v;
+	}
+	return arr
+}
+
+//---------- SHELL SORT
+//fastest of all O(n^2) sorting algos
+
+
+
+
+
+
